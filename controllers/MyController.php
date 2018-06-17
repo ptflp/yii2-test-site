6cbf807429612e10a6d48774aba110fda01cac81
ptflp <filippov.pa@s-vfu.ru>
2018-06-17 13:04:23 +0900

    render variables using compact

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -1,17 +1,19 @@
-<?php 
+<?php
 
 namespace app\controllers;
 use yii\web\Controller;
 /**
-  * 
+  *
   */
  class MyController extends Controller
  {
- 	
+
  	public function actionIndex($id=false)
  	{
- 		$hi='Hello, World';
+ 		$hello='Hello, World';
  		$names=['Petro','Ivanov','Sidorov','Vasiliev'];
- 		return $this->render('index', ['hello'=>$hi,'names'=>$names,'id'=>$id]);
+ 		$id = $id ? $id : 'test';
+ 		//return $this->render('index', ['hello'=>$hi,'names'=>$names,'id'=>$id]);
+ 		return $this->render('index', compact('hello','names','id'));
  	}
  } ?>
