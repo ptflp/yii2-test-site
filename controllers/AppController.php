df037a5396eb783f9b11876394d318aaf536bc55
ptflp <filippov.pa@s-vfu.ru>
2018-06-17 13:49:18 +0900

    creating common controller for SiteController MyController

---
+++ controllers/AppController.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+/**
+  *
+  */
+ class AppController extends Controller
+ {
+ 	public	function actionFoo()
+ 	{
+ 		return 'HAHAH';
+ 	}
+ } ?>
--- controllers/MyController.php
+++ controllers/MyController.php
@@ -5,10 +5,18 @@ use yii\web\Controller;
 /**
   *
   */
- class MyController extends Controller
+ class MyController extends AppController
  {
 
  	public function actionIndex($id=false)
+ 	{
+ 		$hello='Hello, World';
+ 		$names=['Petro','Ivanov','Sidorov','Vasiliev'];
+ 		$id = $id ? $id : 'test';
+ 		//return $this->render('index', ['hello'=>$hi,'names'=>$names,'id'=>$id]);
+ 		return $this->render('index', compact('hello','names','id'));
+ 	}
+ 	public function actionBlogPost($id=false)
  	{
  		$hello='Hello, World';
  		$names=['Petro','Ivanov','Sidorov','Vasiliev'];
