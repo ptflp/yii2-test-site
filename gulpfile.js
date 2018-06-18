bff021269a529a3d0ac41a2694264d358bde43f9
ptflp <filippov.pa@s-vfu.ru>
2018-06-18 22:53:23 +0900

    Gulp 4 reference

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,11 @@
 var
 	gulp 		= require("gulp"),
-	browserSync = require('browser-sync').create(),
-	autoprefixer= require('gulp-autoprefixer'),
-	cleancss	= require('gulp-cleancss'),
-	rename		= require('gulp-rename'),
-	sass		= require("gulp-sass");
+	browserSync = require('browser-sync').create();
 
-gulp.task("reload-css", function () {
-	gulp.src('./src/*.scss')
-	.pipe(sass().on('error',sass.logError))
-	.pipe(autoprefixer({
-		browsers: ['last 3 versions'],
-		cascade: false
-	}))
-	.pipe(cleancss({compatibility: 'ie8'}))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('./src/css/'))
-    .pipe(browserSync.stream());
+gulp.task('watch', function () {
+  gulp.watch("./**/*.php").on('change', gulp.parallel(browserSync.reload));
 });
-gulp.task("console", function () {
-	console.log('php changed');
-});
-gulp.task('serve', ['reload-css'], function() {
-
+gulp.task('ws', function() {
     browserSync.init({
         proxy: "localhost:8000",
 	    snippetOptions: {
@@ -31,11 +14,7 @@ gulp.task('serve', ['reload-css'], function() {
 	        }
 	    }
     });
-
-    gulp.watch("./src/*.scss", ['reload-css']);
-    gulp.watch("./src/*.html").on('change', browserSync.reload);
-    gulp.watch("./**/*.php").on('change', browserSync.reload);
 });
 
 
-gulp.task("default",['serve']);
+gulp.task("default",gulp.parallel('watch','ws'));
