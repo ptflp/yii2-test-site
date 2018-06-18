3f7cc77c73ab7dd4845b8599eeeb0f3861c53182
ptflp <filippov.pa@s-vfu.ru>
2018-06-18 22:59:18 +0900

    set layout for controllers

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -2,12 +2,12 @@
 namespace app\controllers;
 use Yii;
 class PostController extends AppController {
-	public function actionTest(){
-		$names=['Ivanov','Petrov','Sidorov'];
-		$var='';
-		// var_dump($names);
-		// $this->debug($names);
-		// $this->dump($names);
-		return $this->render('test');
+	public $layout = 'basic';
+	public function actionIndex(){
+		return $this->render('index');
+	}
+	public function actionShow(){
+		$this->layout='main';
+		return $this->render('show');
 	}
 }
