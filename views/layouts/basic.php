a9dc4ece149eecf3c8d88c13a09116167e015999
ptflp <filippov.pa@s-vfu.ru>
2018-06-19 14:17:32 +0900

    added yii template functions

--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -1,11 +1,23 @@
+<?php
+use yii\helpers\Html;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
 </head>
 <body>
+	<?php $this->beginBody() ?>
 	<h1>Hello, Basic</h1>
 	<?=$content ?>
+<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
