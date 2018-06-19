b0d1e60aa69ae3e71eaf23c43661889da6cb43c8
ptflp <filippov.pa@s-vfu.ru>
2018-06-19 15:34:35 +0900

    register inline css

--- views/post/show.php
+++ views/post/show.php
@@ -1,3 +1,6 @@
 <h1>Show action</h1>
 <?php $this->registerJsFile('@web/js/scripts.js', ['depends'=>'yii\web\YiiAsset']) ?>
 <?php $this->registerJs("$('.container').append('<p>this->registerJs');", \yii\web\View::POS_LOAD) ?>
+
+
+<?php $this->registerCss(".container{background: #CCC;}") ?>
