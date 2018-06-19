f31ac9e65c9554797c4423f06673287365dfedab
ptflp <filippov.pa@s-vfu.ru>
2018-06-19 23:03:15 +0900

    switch off csrf validation on controller action

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -3,6 +3,13 @@ namespace app\controllers;
 use Yii;
 class PostController extends AppController {
 	public $layout = 'basic';
+	public function beforeAction($action)
+	{
+		if ($action->id == 'index') {
+			$this->enableCsrfValidation = false;
+		}
+		return parent::beforeAction($action);
+	}
 	public function actionIndex($test=false){
 		if (Yii::$app->request->isAjax) {
 			Yii::$app->request->post();
