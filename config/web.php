015dc885ca0656934199a495534ef9607b133335
ptflp <filippov.pa@s-vfu.ru>
2018-06-19 15:27:34 +0900

    switching off debug panel

--- config/web.php
+++ config/web.php
@@ -72,7 +72,7 @@ if (YII_DEBUG) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['127.0.0.1', '::1','172.19.0.1'],
+        'allowedIPs' => ['127.0.0.1', '::1','172.191.0.1'],
     ];
 }
 
