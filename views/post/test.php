607543170bd1e91fdf3a1ded34615769aa2b0832
ptflp <filippov.pa@s-vfu.ru>
2018-06-25 17:05:27 +0900

    view for Form

--- views/post/test.php
+++ views/post/test.php
@@ -1,3 +1,17 @@
+<?php 
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+?>
 <h1>Test Action</h1>
 
-<?=dump(Yii::$app)?>
+<?php /*
+dump($model)
+*/
+
+?>
+<?php $form = ActiveForm::begin() ?>
+	<?= $form->field($model, 'name')->label('Имя')?>
+	<?= $form->field($model, 'email')->input('email') ?>
+	<?= $form->field($model, 'text')->label('Текст сообщения')->textarea(['rows' => 5])?>
+	<?= Html::submitButton('Отправить',['class'=>'btn btn-success']) ?>
+<?php ActiveForm::end() ?>
